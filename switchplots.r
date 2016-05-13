545eed62d2383d22270c865b5c1e7e978e1ca13d
garrett <garretthonke@gmail.com>
2016-05-13 17:26:59 -0400

    updated final figs cogsci2016

--- switchplots.r
+++ switchplots.r
@@ -67,7 +67,7 @@ ggplot(aggregate(test_acc ~ train_cond + eg_type + pid, mean,
 dev.off()
 
 
-tikz('switch_test.tex', width = 2.5, height = 2.5, pointsize = 10, sanitize = TRUE)
+tikz('switch_test.tex', width = 3, height = 2.5, pointsize = 10, sanitize = TRUE)
 
 ggplot(aggregate(test_acc ~ train_cond + pid, mean, 
     data = subset(datafull, subset = (phase == 'switch_test'))), 
@@ -76,6 +76,7 @@ ggplot(aggregate(test_acc ~ train_cond + pid, mean,
     alpha = .7, size = .75) +
   stat_summary(fun.y = mean, geom = "point", shape = 18, colour = 'black', size = 2,
     show.legend = FALSE) +
+  facet_wrap(~ train_cond, labeller = as_labeller(cond_names), scales = 'free_x') +
   scale_colour_manual('task', values = c('classify' = 'black','switch' = 'black')) +
   scale_fill_manual('task', values = c('classify' = 'light grey','switch' = 'light grey')) +
   scale_x_discrete(name = '', breaks = c('classify', 'switch'), labels = c('Classify', 'Switch')) + 
@@ -83,19 +84,22 @@ ggplot(aggregate(test_acc ~ train_cond + pid, mean,
     axis.title.y = element_text(size = 9.5, colour = 'black'),
     axis.title.x = element_blank(),
     axis.text.y = element_text(size = 8, colour = 'black'),
-    axis.text.x = element_text(size = 9.5, colour = 'black'),
-    strip.text.x = element_text(size = 10, colour = 'black'), 
+    axis.text.x = element_blank(),
+    strip.text.x = element_text(size = 10, colour = 'black'),
+    axis.ticks.x = element_blank(),
+    panel.grid.major.x = element_blank(), 
     plot.margin = unit(c(0,0,.1,0), "cm"),
     legend.position = 'none') +
   scale_y_continuous(limits = c(0,1.00), breaks = seq(0, 1, 0.2), name = 'Mean Proportion Correct') +
   coord_cartesian(ylim = c(.03,.97))
 
+ 
 dev.off()
-
-tikz('training_fig.tex', width = 7, height = 3, pointsize = 10, sanitize = TRUE)
 
 training_data$shj_cond <- factor(training_data$shj_cond, c("two","three","four"))
 levels(training_data$shj_cond) <- c('II', 'III', 'IV')
+
+tikz('training_fig.tex', width = 7.75, height = 3, pointsize = 10, sanitize = TRUE)
 
 acc_x_block_plot_swi <- ggplot(data = aggregate(var_19 ~ block_num + shj_cond + pid,
     mean, data = subset(training_data, 
@@ -104,11 +108,14 @@ acc_x_block_plot_swi <- ggplot(data = aggregate(var_19 ~ block_num + shj_cond + 
   geom_boxplot(outlier.colour = NA, lwd = 1, fatten = 1) +
   scale_x_discrete(name = '') + scale_y_continuous(name = 'Switch', 
     limits = c(0,1.01), breaks = seq(0, 1, .25),) +
-  scale_fill_brewer('Category Type', palette = 'Purples') +
-  theme(axis.text.y = element_text(size = 8, color = 'black'), axis.text.x = element_blank(),
-    axis.title.y = element_text(size = 10), legend.title = element_blank(), 
-    legend.text = element_text(size = 16),
-    plot.margin = unit(c(0,0,0,0), "cm"))
+  scale_fill_brewer(name = 'Category\nStructure', palette = 'Purples') +
+  theme(axis.text.y = element_text(size = 8, color = 'black'), 
+    axis.text.x = element_blank(),
+    axis.title.x = element_text(size = 10, color = 'black'), 
+    axis.title.y = element_text(size = 10),
+    legend.title = element_text(size = 10), 
+    legend.text = element_text(size = 10),
+    plot.margin = unit(c(.2,0,0,0), "cm"))
 
 acc_x_block_plot_cla <- ggplot(data = aggregate(var_19 ~ block_num + shj_cond + pid,
     mean, data = subset(training_data, 
@@ -117,11 +124,13 @@ acc_x_block_plot_cla <- ggplot(data = aggregate(var_19 ~ block_num + shj_cond + 
   geom_boxplot(outlier.colour = NA, lwd = 1, fatten = 1) +
   scale_x_discrete(name = 'Block Number') + scale_y_continuous(name = 'Classify', 
     limits = c(0,1.01), breaks = seq(0, 1, .25),) +
-  scale_fill_brewer('Category Type', palette = 'Purples', breaks = c('II', 'III', 'IV')) +
+  scale_fill_brewer(name = 'Category\nStructure', palette = 'Purples') +
   theme(axis.text.y = element_text(size = 8, color = 'black'), 
     axis.text.x = element_text(size = 8, color = 'black'),
-    axis.title.x = element_text(size = 10, color = 'black', margin = margin(10,0,0,20)), 
-    axis.title.y = element_text(size = 10),  
+    axis.title.x = element_text(size = 10, color = 'black'), 
+    axis.title.y = element_text(size = 10),
+    legend.title = element_text(size = 10), 
+    legend.text = element_text(size = 10),
     plot.margin = unit(c(.2,0,0,0), "cm")) 
 
 gt1 <- ggplot_gtable(ggplot_build(acc_x_block_plot_swi))
